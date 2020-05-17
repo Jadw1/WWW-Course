6c2268c6d3dfcbbdca048e50d233386a8ccd9933
Jadw1 <jadw1_1@o2.pl>
2020-05-17 12:05:21 +0200

    lab9

---
+++ lab9/app.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import { Meme, MemesStorage } from './meme';
+
+const most_expensive = [
+    {
+        'id': 10,
+        'name': 'Gold',
+        'price': 1000,
+        'url': 'https://i.redd.it/h7rplf9jt8y21.png'
+    },
+    {
+        'id': 9,
+        'name': 'Platinum',
+        'price': 1100,
+        'url': 'http://www.quickmeme.com/img/90/90d3d6f6d527a64001b79f4e13bc61912842d4a5876d17c1f011ee519d69b469.jpg'
+    },
+    {
+        'id': 8,
+        'name': 'Elite',
+        'price': 1200,
+        'url': 'https://i.imgflip.com/30zz5g.jpg'
+    }
+];
+
+const app = express();
+const storage = new MemesStorage();
+
+most_expensive.forEach(m => {
+    storage.addMeme(new Meme(m.id, m.name, m.price, m.url));
+});
+
+app.set('view engine', 'pug');
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Meme market', message: 'Hello there!', storage: storage })
+});
+
+app.get('/meme/:memeId(\\d+)', function (req, res, next) {
+    let meme = get_meme(parseInt(req.params.memeId));
+    if(!meme)
+        next();
+    res.render('meme', { meme: meme })
+ });
+
+ app.use((req, res) => {
+     res.status(404);
+     res.render('404');
+ })
+
+const server = app.listen(1500, () => {
+    console.log(`App is running at http://localhost:1500/ in ${app.get('env')} mode`);
+    console.log('Press Ctrl+C to stop.');
+});
---
+++ lab9/meme.ts
@@ -0,0 +1,53 @@
+export class Meme {
+    id: number;
+    name: string;
+    price: number;
+    url: string;
+    pricesHistory: number[];
+
+    constructor(id: number, name: string, price: number, url: string) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.url = url;
+    }
+
+    setPrice(price: number) {
+        this.pricesHistory.push(this.price);
+        this.price = price;
+    }
+}
+
+export class MemesStorage {
+    memes: Meme[];
+
+    constructor() {
+        this.memes = [];
+    }
+
+    addMeme(meme: Meme) {
+        this.memes.push(meme);
+    }
+
+    addMemes(memes: Meme[]) {
+        this.memes = this.memes.concat(memes);
+    }
+
+    getTop3(): Meme[] {
+        const sorted = this.memes.sort((m1, m2) => {
+            return m1.price > m2.price ? -1 : 1;
+        });
+
+        return sorted.slice(0, 3);
+    }
+
+    getMeme(id: number): Meme {
+        this.memes.forEach(meme => {
+            if(meme.id === id) {
+                return meme;
+            }
+        });
+
+        return null;
+    }
+}
